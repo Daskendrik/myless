8f366a15ce4da3c1b8cf901cd105474ac7d69a1c
Daskendrik <Daskendrik@users.noreply.github.com>
2019-08-05 18:57:34 +0300

    Операции типа +-*/

---
+++ src/main/java/dahka/exampleOpp.java
@@ -0,0 +1,33 @@
+package dahka;
+
+public class exampleOpp {
+    public static void main (String[] args){
+
+        String str1 = "Целочисленная арифметика";
+        System.out.println(str1);
+        int a = 1 + 1;
+        int b = a * 3;
+        int c = b / 4;
+        int d = c - a;
+        int e = -d;
+        System.out.println("a = " + a );
+        System.out.println("b = " + b );
+        System.out.println("c = " + c );
+        System.out.println("d = " + d );
+        System.out.println("e = " + e );
+
+        str1 = "Арифметика с плавующей точкой";
+        System.out.println("\n" +str1);
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = db / 4;
+        double dd = dc - a;
+        double de = - dd;
+        System.out.println("da = " + da );
+        System.out.println("db = " + db );
+        System.out.println("dc = " + dc );
+        System.out.println("dd = " + dd );
+        System.out.println("de = " + de );
+
+    }
+}
