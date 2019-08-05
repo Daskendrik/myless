e72bdfdda1025b25cde702c24fb0a41a62b89bad
Daskendrik <Daskendrik@users.noreply.github.com>
2019-08-05 18:57:18 +0300

    mod деление с остатком

---
+++ src/main/java/dahka/ExampleDelenie.java
@@ -0,0 +1,11 @@
+package dahka;
+
+public class ExampleDelenie {
+    public static void main (String[] args) {
+
+        int x = 42;
+        double y = 42.24;
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y mod 10 = " + y % 10); // есть над чем подумать
+    }
+}
