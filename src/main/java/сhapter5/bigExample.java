d086682bfbd448d358d3d84c4b386f55ad59b640
Daskendrik <Daskendrik@users.noreply.github.com>
2019-08-16 17:23:41 +0300

    Крутой пример выбора и описаний операторов

---
+++ src/main/java/сhapter5/bigExample.java
@@ -0,0 +1,51 @@
+package сhapter5;
+
+public class bigExample {
+    public static void main (String[] args)
+    throws java.io.IOException{
+
+        char choice;
+        do {
+            System.out.println("help operator");
+            System.out.println("    1. if");
+            System.out.println("    2. switch");
+            System.out.println("    3. while");
+            System.out.println("    4. do-while");
+            System.out.println("    5. for ");
+            System.out.println(" select the desired item ");
+            choice = (char) System.in.read();
+        } while (choice<'1' || choice>'5');
+
+        switch (choice) {
+            case '1':
+                System.out.println("if:");
+                System.out.println("if(условеи) оператор;");
+                System.out.println("else оператор;");
+                break;
+            case '2':
+                System.out.println("switch:");
+                System.out.println("switch (выражение) {");
+                System.out.println(" case константа:");
+                System.out.println(" breake;");
+                System.out.println("// ...");
+                System.out.println("}");
+                break;
+            case '3':
+                System.out.println("while: ");
+                System.out.println("while (условие) оператор;");
+                break;
+            case '4':
+                System.out.println("do-while: ");
+                System.out.println("do {");
+                System.out.println("    оператор;");
+                System.out.println("} while (условеи)");
+                break;
+            case '5':
+                System.out.println("for: ");
+                System.out.println("for (инициализация; условеи; итерация) ");
+                System.out.println(" оператор");
+                break;
+        }
+
+    }
+}
