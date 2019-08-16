8515eead3c60081b270ee1b5d381c7b2c278c27b
Daskendrik <Daskendrik@users.noreply.github.com>
2019-08-16 12:58:55 +0300

    Пример Do While

---
+++ src/main/java/сhapter5/exampleDoWhile.java
@@ -0,0 +1,12 @@
+package сhapter5;
+
+public class exampleDoWhile {
+    public static void main (String[] args) {
+
+        int n = 10;
+        do {
+            System.out.println("Step " + n);
+            n--;
+        } while (n>0);
+    }
+}
