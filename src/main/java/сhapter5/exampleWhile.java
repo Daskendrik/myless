13bf0f46d53fe1f18360e0d9f78ad9f5b1aacd4c
Daskendrik <Daskendrik@users.noreply.github.com>
2019-08-16 12:58:26 +0300

    Примерчик while

---
+++ src/main/java/сhapter5/exampleWhile.java
@@ -0,0 +1,20 @@
+package сhapter5;
+
+public class exampleWhile {
+    public static void main (String[] args) {
+
+        int n = 10;
+        while (n>0){
+            System.out.println("Tik tak number " + n);
+            n--;
+        }
+        System.out.println(" ");
+        int i, j;
+        i = 100;
+        j = 200;
+
+        //Расчитаем средене значение переменной  ( но скорее это медиана)
+        while (++i < --j);
+        System.out.println("average value is " + i);
+    }
+}
