d397fe39e1989537de2dfad2f3830b322542b88f
Daskendrik <52924734+Daskendrik@users.noreply.github.com>
2019-08-17 22:27:24 +0300

    кусок из игры

---
+++ src/main/java/mygame/bullAndCows/validationFullNumber.java
@@ -0,0 +1,12 @@
+package mygame.bullAndCows;
+
+public class validationFullNumber {
+    public static void main (String[] args)
+            throws java.io.IOException{
+        int fullNumber;
+        fullNumber = System.in.read();
+
+
+
+    }
+}
