c8f874ae99e4dca961694e2cea71c6ff52ae8662
Daskendrik <52924734+Daskendrik@users.noreply.github.com>
2019-08-18 14:37:44 +0300

    пример клас-го for

---
+++ src/main/java/сhapter5/exampleFor.java
@@ -0,0 +1,11 @@
+package сhapter5;
+
+public class exampleFor {
+    public static void main (String[] args) {
+
+        for (int n = 10; n>0; n--) { //Мы задаем переменную прямо в цикле for, живет она только в нем
+            System.out.println("Step " + n);
+        }
+
+    }
+}
