9b7bb37ecf6d4486a50ed56dd30e747d6419a449
Daskendrik <Daskendrik@users.noreply.github.com>
2019-08-20 17:25:55 +0300

    Добавила проверку числа и цифр

--- src/main/java/mygame/bullAndCows/bullANDcows.java
+++ src/main/java/mygame/bullAndCows/bullANDcows.java
@@ -1,39 +1,41 @@
 package mygame.bullAndCows;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // Игры быки и коровы, была реализована в С++, ща пробую в Java
 public class bullANDcows {
     public static void main (String[] args)
             throws java.io.IOException{
 
        // int cow, bulls; // коровы это значит, цифра есть в числе, бык значит, что цифра есть и она на своем месте
-        int fullNumber; // число которое введут и надо угадать
+        BufferedReader fullNumber = new BufferedReader(new InputStreamReader(System.in)); // число которое введут и надо угадать
+        int numberOne = 0;
+
         int num1, num2, num3, num4; // цифры, которые надо угадать
         //int tryNum1, tryNum2, tryNum3, tryNum4; // цифры, которые будет вводить пользователь
-        boolean a,b; // Для проверки введенного числа
 
-        System.out.println("Введите загаданное число");
 
+        boolean[] arrayResult;
         do {
-            fullNumber = (char) System.in.read();
-
-            if (fullNumber < 999 || fullNumber >10000) {
-                System.out.println(fullNumber + " не 4х значное!");
-                a = false;
-            } else {
-                a = true;
+            System.out.println("Введите загаданное число");
+            try{ //тут начинается какая то дичь, надо сюда вернуться, когда дойду до главы соответствующей
+                String s = fullNumber.readLine();
+                numberOne = Integer.parseInt(s);
+                System.out.println(numberOne);
+            }catch (Exception ex){
+                ex.printStackTrace();
             }
-            num4 = fullNumber%10;
-            num3 = fullNumber/10%10;
-            num2 = fullNumber/100%10;
-            num1 = fullNumber/1000;
-            System.out.println("1 число " + num1 + "\n2 число "+ num2 + "\n3 число " + num3 + "\n1 число " + num4); //Проверяем, что места цифр определены правильно
-            if (num1==num2 || num1==num3 || num1==num4 || num2==num3 || num2==num4 || num3==num4) {
-                System.out.println("Введенное число содержит повторяющиеся цифры!");
-                b = false;
-            } else {
-                b = true;
+             num4 = numberOne%10; // определяем 4 цифру числа
+             num3 = numberOne/10%10; // определяем 3 цифру числа
+             num2 = numberOne/100%10; // определяем 2 цифру числа
+             num1 = numberOne/1000;// определяем  цифру 1 числа
+            int[] checkNumber = {numberOne, num1, num2, num3, num4 }; // Создаем массив из числа и цифр
+             arrayResult = validationFullNumber.checkNumber(checkNumber); //отправляем на проверку число и цифры
+            for(int i=0; i < 2; i++){
+                System.out.println(arrayResult[i]);
             }
-
-        } while (a == false || b == false);
+        } while ( arrayResult[0] == false ||  arrayResult[1] == false);
 
         System.out.println("Надо угадать число " + fullNumber);
     }
