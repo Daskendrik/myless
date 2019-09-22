dea258e41cf25f677733a116a95e417bef330a84
Daskendrik <52924734+Daskendrik@users.noreply.github.com>
2019-09-22 14:07:20 +0300

    Return

---
+++ src/main/java/сhapter5/oprReturn/Return.java
@@ -0,0 +1,13 @@
+package сhapter5.oprReturn;
+
+public class Return {
+    public static void main (String[] args) {
+
+        boolean t = true;
+        System.out.println("До возрата");
+
+        if (t) return;
+        System.out.println("Это мы уже не сможем увидеть");
+
+    }
+}
