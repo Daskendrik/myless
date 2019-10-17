5d4dfe8855c59047b13acac3a7fdfc7fdeb924c3
Daskendrik <Daskendrik@users.noreply.github.com>
2019-10-17 18:02:21 +0300

    расчет комнаты

--- src/main/java/chapter6/myexp/SomeForm.java
+++
@@ -1,9 +0,0 @@
-package chapter6.myexp;
-//тут будет расчет стоимости квартиры в зависимости от количества комнат, ширины и длины.
-public class SomeForm {
-    double width;
-    double depth;
-    int room;
-    int costForPM;
-
-}
