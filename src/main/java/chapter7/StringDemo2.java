65958a9d5e00a0220f0b7c1089faaf0176d64b14
Daskendrik <Daskendrik@users.noreply.github.com>
2019-11-06 18:20:17 +0300

    String массив

---
+++ src/main/java/chapter7/StringDemo2.java
@@ -0,0 +1,10 @@
+package chapter7;
+
+public class StringDemo2 {
+    public static void main (String[] args){
+        String[] str = {"own", "two", "tree"};
+
+        for (int i=0; i<str.length; i++)
+            System.out.println("str[" + i +"]: " + str[i]);
+    }
+}
