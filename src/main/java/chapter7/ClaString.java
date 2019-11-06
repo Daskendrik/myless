a7a3e84b27c9492c17c8adeba9bc424448e442f9
Daskendrik <Daskendrik@users.noreply.github.com>
2019-11-06 17:23:05 +0300

    класс String и ненмного его методов

---
+++ src/main/java/chapter7/ClaString.java
@@ -0,0 +1,13 @@
+package chapter7;
+
+public class ClaString {
+    public static void main (String[] args) {
+        String a = "Первая строчка";
+        String b = "Вторая строчка";
+        String c = a + " И " + b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ src/main/java/chapter7/StringDemo.java
@@ -0,0 +1,21 @@
+package chapter7;
+
+public class StringDemo {
+    public static void main (String[] args) {
+        String a = "Первая строчка";
+        String b = "Вторая строчка";
+        String c = a;
+        System.out.println("Длина строки a: " + a.length());
+        System.out.println("Символ по индексу 3 в строке a: " + a.charAt(3));
+        if(a.equals(b))
+            System.out.println("a==b");
+        else
+            System.out.println("a!=b");
+
+        if (a.equals(c))
+            System.out.println("a==c");
+        else
+            System.out.println("a!=c");
+
+    }
+}
