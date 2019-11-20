9c151233c7a6f456172faffe3015c96d3a6a238d
Daskendrik <Daskendrik@users.noreply.github.com>
2019-11-20 16:44:15 +0300

    Неоднозначность переменных аргументов

--- src/main/java/chapter7/VaArg2.java
+++ src/main/java/chapter7/VaArg2.java
@@ -6,6 +6,11 @@ public class VaArg2 {
         for (int x : v) System.out.print(x + " ");
         System.out.println();
     }
+    static void vaTest(int a, int ... v) {
+        System.out.print("vaTest(int ...) " + "Количество аргументов " + v.length + " Содержимое ");
+        for (int x : v) System.out.print(x + " ");
+        System.out.println();
+    }
 
     static void vaTest(boolean ... v) {
         System.out.print("vaTest(boolean ...) " + "Количество аргументов " + v.length + " Содержимое ");
@@ -19,10 +24,13 @@ public class VaArg2 {
         System.out.println();
     }
 
+
     public static void main (String[] args) {
         vaTest(1, 2, 3);
         vaTest("Проверка", "10", "20");
         vaTest(true, false,false,true,true,false);
         vaTest();
+        vaTest(2);
+
     }
 }
