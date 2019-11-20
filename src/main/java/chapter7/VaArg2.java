0401cb47e048c8cb4cd846d21b3f0f27704ccb63
Daskendrik <Daskendrik@users.noreply.github.com>
2019-11-20 16:42:11 +0300

    не скомпилится
    связано с тем, что пусто значение может принадлежать 2 методам

---
+++ src/main/java/chapter7/VaArg2.java
@@ -0,0 +1,28 @@
+package chapter7;
+
+public class VaArg2 {
+    static void vaTest(int ... v) {
+        System.out.print("vaTest(int ...) " + "Количество аргументов " + v.length + " Содержимое ");
+        for (int x : v) System.out.print(x + " ");
+        System.out.println();
+    }
+
+    static void vaTest(boolean ... v) {
+        System.out.print("vaTest(boolean ...) " + "Количество аргументов " + v.length + " Содержимое ");
+        for (boolean x : v) System.out.print(x + " ");
+        System.out.println();
+    }
+
+    static void vaTest(String ... v) {
+        System.out.print("vaTest(String ...) " + "Количество аргументов " + v.length + " Содержимое ");
+        for (String x : v) System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main (String[] args) {
+        vaTest(1, 2, 3);
+        vaTest("Проверка", "10", "20");
+        vaTest(true, false,false,true,true,false);
+        vaTest();
+    }
+}
