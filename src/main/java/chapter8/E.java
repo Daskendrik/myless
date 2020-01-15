975548338102226c24e42cc9a4af7fb815f8de11
Daskendrik <52924734+Daskendrik@users.noreply.github.com>
2020-01-15 23:02:13 +0300

    Интересный пример переопределения методов при наследовании

--- src/main/java/chapter8/E.java
+++ src/main/java/chapter8/E.java
@@ -1,5 +1,33 @@
 package chapter8;
 
 public class E {
+    int i, j;
+    E(int a, int b){
+        i = a;
+        j = b;
+    }
 
+    void show(){
+        System.out.println("i и j: " + i +" " + j);
+    }
+}
+
+class T extends E{
+    int k;
+    T(int q, int w, int x) {
+        super(q, w);
+        k = x;
+    }
+
+    void show(String msg){
+        System.out.println(msg + k);
+    }
+}
+
+class DemoET{
+    public static void main (String[] args){
+        T t = new T(1,2,3);
+        t.show("Это k: ");  //Тут вызываем шоу из класса T, так как передали сообщение
+        t.show();                 //Тут вызываем шоу из класса E, так как пусто
+    }
 }
